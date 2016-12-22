a0a7aebdf9a76ef1f4c4db84e6fa7047aae19a7b
Desgard_Duan <desgard_duan@foxmail.com>
2016-12-22 15:36:23 +0800

    Initial db by model(using sqlite3)

--- Ranker/RankList/models.py
+++ Ranker/RankList/models.py
@@ -3,3 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length = 30)
+    codeforces_id = models.CharField(max_length = 30)
+    codeforces_rating = models.IntegerField()
+    bestcoder_id = models.CharField(max_length = 30)
+    bestcoder_rating = models.IntegerField()
+    hdu_id = models.CharField(max_length = 30)
+    hdu_problems = models.IntegerField()
+    poj_id = models.CharField(max_length = 30)
+    poj_problems = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
