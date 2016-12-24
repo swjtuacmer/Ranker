cbee385dd3ed3ec6d2a9a0d6eef43cdf806b6503
ryotakx <470936130@qq.com>
2016-12-24 14:19:17 +0800

    upadate crawler
    issue #13

--- Ranker/RankList/dc/crawler.py
+++ Ranker/RankList/dc/crawler.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import urllib2
 import json
+import requests
+from bs4 import BeautifulSoup
+import re
 
 # 获取 codeforces rating 数据
 def UpdateCodeforces(codeforces_id):
@@ -14,16 +17,44 @@ def UpdateCodeforces(codeforces_id):
 
 # 获取 BestCoder rating 数据
 def UpdateBestCoder(bestcoder_id):
-    res_rating = 0
+    url_bestcoder = "http://bestcoder.hdu.edu.cn/ranklist.php?type=username&keyword="
+    try:
+        data = requests.get(url_bestcoder + bestcoder_id)
+        soup = BeautifulSoup(data.text, "html.parser")
+        # print(soup.prettify())
+        links = soup.find_all("td")[-1].string
+        res_rating = int(links)
+    except:
+        res_rating = 0
     return res_rating
+
 
 # 获取 HDU 数据
 def UpdateHduProblems(hdu_id):
-    res_problems = 0
+    url_hdu = "http://acm.hdu.edu.cn/search.php?field=author&key="
+    try:
+        data = requests.get(url_hdu + hdu_id)
+        soup = BeautifulSoup(data.text, "html.parser")
+        #print(soup.prettify())
+        links = str(soup.find_all("tbody")[0])
+        pattern = re.compile(r"(</td><td>)(\d+)(</td>)")
+        match = re.search(pattern, links)
+        res_problems = int(match.group(2))
+    except:
+        res_problems = 0
     return res_problems
 
 # 获取 POJ 数据
 def UpdatePojProblems(poj_id):
-    res_problems = 0
+    url_poj = "http://poj.org/searchuser?key=***&B1=Search"
+    try:
+        url = url_poj.replace("***", poj_id)
+        data = requests.get(url)
+        soup = BeautifulSoup(data.text, "html.parser")
+        # print(soup.prettify())
+        links = soup.find_all("td")[-2].string
+        res_problems = int(links)
+    except:
+        res_problems = 0
     return res_problems
 
