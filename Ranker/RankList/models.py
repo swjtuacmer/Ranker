a032822c1f2fc9bde44c55760f343ae69baf07f6
Desgard_Duan <desgard_duan@foxmail.com>
2016-12-25 14:14:11 +0800

    Update sqlite form - add vj msg

--- Ranker/RankList/models.py
+++ Ranker/RankList/models.py
@@ -6,16 +6,17 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length = 30)
-    codeforces_id = models.CharField(max_length = 30)
-    codeforces_rating = models.IntegerField()
-    bestcoder_id = models.CharField(max_length = 30)
-    bestcoder_rating = models.IntegerField()
-    hdu_id = models.CharField(max_length = 30)
-    hdu_problems = models.IntegerField()
-    poj_id = models.CharField(max_length = 30)
-    poj_problems = models.IntegerField()
+    codeforces_id = models.CharField(max_length = 30, null = True)
+    codeforces_rating = models.IntegerField(default = 0, null = True)
+    bestcoder_id = models.CharField(max_length = 30, null = True)
+    bestcoder_rating = models.IntegerField(default = 0, null = True)
+    hdu_id = models.CharField(max_length = 30, null = True)
+    hdu_problems = models.IntegerField(default = 0, null = True)
+    poj_id = models.CharField(max_length = 30, null = True)
+    poj_problems = models.IntegerField(default = 0, null = True)
+    vj_id = models.CharField(max_length = 30, null = True)
+    vj_problems = models.IntegerField(default = 0, null = True)
 
     def __unicode__(self):
         return self.name
-
 
